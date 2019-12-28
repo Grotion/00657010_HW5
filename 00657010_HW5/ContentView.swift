501c572fa1eb11e0af5e1bfde67077162c31ed9b
GroTion <grotion0720@gmail.com>
2019-12-28 16:22:15 +0800

    Initial Commit

---
+++ 00657010_HW5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00657010_HW5
+//
+//  Created by Shaun Ku on 2019/12/28.
+//  Copyright © 2019 Shaun Ku. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
